c902c303703529149a044ec0644daee876f8dbd3
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2025-12-19 00:54:02 +0100

    Ajout de la connection avec la base de données

---
+++ class/DBconnect.php
@@ -0,0 +1,24 @@
+<?php
+class DBconnect {
+    private $host = '127.0.0.1';
+    private $db_name = 'exercice_1_poo';
+    private $user = 'root';
+    private $pass = '';
+    private $pdo;
+    private $db;
+
+    // Méthode de connexion
+    public function connect() {
+        try {
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';charset=utf8mb4';
+
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $this->pdo;
+        } catch (Exception $exception) {
+            echo 'Erreur de connexion : ' . $exception->getMessage();
+            exit;
+        }
+    }
+}
--- main.php
+++ main.php
@@ -1,5 +1,10 @@
 <?php
+require "class/DBconnect.php";
 while (true) {
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
+    if ($line === 'list') {
+        echo "Affichage de la liste \n";
+    } else {
+        echo "Votre commande est : $line \n";
+    }
 }
