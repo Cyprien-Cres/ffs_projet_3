b60e19f59b1ce8c7bf9b045f3baef873038716f5
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2025-12-20 00:49:03 +0100

    Modification de la classe pour gérer la logique des commandes

--- class/Command.php
+++ class/Command.php
@@ -7,19 +7,6 @@ class Command {
                 $contacts = new Contact();
                 $contact = $contacts->toString();
                 echo $contact . "\n";
-            } elseif (preg_match('/^detail(?:\s+(\d+))?$/', $line, $matches)) {
-                $id = isset($matches[1]) && $matches[1] !== '' ? $matches[1] : trim(readline("Entrez l'id du contact : "));
-                if ($id === '') {
-                    echo "Aucun id fourni.\n";
-                    continue;
-                }
-                $contacts = new Contact();
-                $contact = $contacts->toString($id);
-                if ($contact === null || $contact === '') {
-                    echo "Contact introuvable pour l'id $id.\n";
-                } else {
-                    echo $contact . "\n";
-                }
             } else {
                 echo "Votre commande : $line , n'est pas reconnue. Veuillez réessayer.\n";
             }
