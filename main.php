006f39ca0e48e1865a21af4cd72ee49bb0b49fb8
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2025-12-20 00:35:17 +0100

    Création de la classe, DBConnect, ContactManager, Contact.

--- main.php
+++ main.php
@@ -2,13 +2,7 @@
 require "class/DBconnect.php";
 require 'class/ContactManager.php';
 require 'class/Contact.php';
-while (true) {
-    $line = readline("Entrez votre commande : ");
-    if ($line === 'list') {
-        echo "Affichage de la liste \n";
-        $contacts = new Contact();
-        $contact = $contacts->toString();
-    } else {
-        echo "Votre commande est : $line \n";
-    }
-}
+require 'class/Command.php';
+
+$command = new Command();
+$command->command();
