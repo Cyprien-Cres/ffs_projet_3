124d7bfe4d14c93c28cdbe111e5a8fd1cf6c57be
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2025-12-19 02:21:34 +0100

    Ajout de la class Contact et ContactManager

--- class/Contact.php
+++ class/Contact.php
@@ -1,8 +1,33 @@
 <?php
+require_once 'DBconnect.php';
+require_once 'ContactManager.php';
+class Contact {
+    public function getId() {
+        $contactManager = new ContactManager();
+        $contacts = $contactManager->findAll();
 
-namespace Cyprien;
+        foreach ($contacts as $contact) {
+            echo "ID: " . $contact['id'] . "\n";
+        }
+    }
+    public function getName() {
+        $contactManager = new ContactManager();
+        $contacts = $contactManager->findAll();
 
-class Contact
-{
+        foreach ($contacts as $contact) {
+            echo "Name: " . $contact['name'] . "\n";
+        }
+    }
 
+    public function toString() {
+        $contactManager = new ContactManager();
+        $contacts = $contactManager->findAll();
+
+        foreach ($contacts as $contact) {
+            echo "Contact : "  . "\n" . " - ID : " . $contact['id']
+                . "\n" . " - Nom : " . $contact['name']
+                . "\n" . " - Email : " . $contact['email']
+                . "\n" . " - Numéro téléphone : " . $contact['phone_number'] . "\n";
+        }
+    }
 }
--- main.php
+++ main.php
@@ -1,9 +1,13 @@
 <?php
 require "class/DBconnect.php";
+require 'class/ContactManager.php';
+require 'class/Contact.php';
 while (true) {
     $line = readline("Entrez votre commande : ");
     if ($line === 'list') {
         echo "Affichage de la liste \n";
+        $contacts = new Contact();
+        $contact = $contacts->toString();
     } else {
         echo "Votre commande est : $line \n";
     }
