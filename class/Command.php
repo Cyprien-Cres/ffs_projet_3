590fe8559eb284fab335ad7a33b1cc261c748a53
Cyprien Cres <140599994+Cyprien-Cres@users.noreply.github.com>
2025-12-20 02:21:49 +0100

    Ajout de la commande Detail

--- class/Command.php
+++ class/Command.php
@@ -7,6 +7,19 @@ class Command {
                 $contacts = new Contact();
                 $contact = $contacts->toString();
                 echo $contact . "\n";
+            } elseif (preg_match('/^detail(?:\s+(\d+))?$/', $line, $matches)) {
+                $id = isset($matches[1]) && $matches[1] !== '' ? $matches[1] : trim(readline("Entrez l'id du contact : "));
+                if ($id === '') {
+                    echo "Aucun id fourni.\n";
+                    continue;
+                }
+                $contact = new Contact();
+                if ($contact === null || $contact === '') {
+                    echo "Contact introuvable pour l'id $id.\n";
+                } else {
+                    $contactData = $contact->toString($id);
+                    echo $contactData . "\n";
+                }
             } else {
                 echo "Votre commande : $line , n'est pas reconnue. Veuillez réessayer.\n";
             }
